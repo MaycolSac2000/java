8c9e117548cafeb3a58d3d2fc888d81e06338897
MaycolSac2000 <ixinversorsmaycol@gmail.com>
2025-11-12 13:58:32 -0500

    Se agrega las clases Cuadrado y el main TestCuadrado con sus respectivos metodos area y perimetro funcionando ademas las capturas en word

---
+++ Clases/src/com/krakedev/Cuadrado.java
@@ -0,0 +1,26 @@
+package com.krakedev;
+
+public class Cuadrado {
+
+	public double lado;
+	
+	
+	public double calcularArea() {
+		
+		double area;
+		area = Math.pow(lado, 2);
+		return area;
+	
+	}
+	
+	
+	public double calcularPerimetro() {
+		
+		double perimetro = lado *4;
+		return perimetro;
+		
+		
+	}
+	
+	
+}
---
+++ Clases/src/com/krakedev/testCuadrado.java
@@ -0,0 +1,44 @@
+package com.krakedev;
+
+public class testCuadrado {
+
+	public static void main(String[] args) {
+		
+		Cuadrado c1 = new Cuadrado();
+		Cuadrado c2 = new Cuadrado();
+		Cuadrado c3 = new Cuadrado();
+		
+		double areaCuadrado1;
+		double areaCuadrado2;
+		double areaCuadrado3;
+		double perimetroCuadrado1;
+		double perimetroCuadrado2;
+		double perimetroCuadrado3;
+		
+		
+		
+		c1.lado = 4;
+		c2.lado = 6;
+		c3.lado = 45;
+		
+		areaCuadrado1 = c1.calcularArea();
+		areaCuadrado2 = c2.calcularArea();
+		areaCuadrado3 = c3.calcularArea();
+		
+		perimetroCuadrado1 = c1.calcularPerimetro();
+		perimetroCuadrado2 = c2.calcularPerimetro();
+		perimetroCuadrado3 = c3.calcularPerimetro();
+		
+		System.out.println("Area Cuadrado 1: "+areaCuadrado1);
+		System.out.println("Area Cuadrado 2: "+areaCuadrado2);
+		System.out.println("Area Cuadrado 3: "+areaCuadrado3);
+
+		System.out.println("Perimetro Cuadrado 1: "+perimetroCuadrado1);
+		System.out.println("Perimetro Cuadrado 2: "+perimetroCuadrado2);
+		System.out.println("Perimetro Cuadrado 3: "+perimetroCuadrado3);
+		
+		
+		
+	}
+
+}
